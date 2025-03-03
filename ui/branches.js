9c0e272f08bb03cff1b3189b28a16f5e056d7767
Abhinav Goyal <abhinavgoel@ONDC-DEL-M012.local>
2025-03-03 11:56:51 +0530

    updated

--- ui/branches.js
+++ ui/branches.js
@@ -2,13 +2,13 @@ const BRANCHES = [
   {
     code: "main",
     name: "Logistics 1.2.5",
-    short_desc: "Logistics Specifications",
+    short_desc: "Logistics Specifications v1.2.5",
     status: "DRAFT",
   },
   {
     code: "draft-2.x",
     name: "draft Logistics 2.x",
-    short_desc: "draft Logistics 2.x",
+    short_desc: "Logistics Specification v2.x",
     status: "DRAFT",
   },
 ];
